0f481583c22bc62f8703793d59c7e3bbbfab4c6d
Victor Mikell <78280727+vmikell@users.noreply.github.com>
2021-07-06 22:41:05 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './Portfolio';
 import Header from './Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import $ from 'jquery'
-import Popper from 'popper.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import reportWebVitals from './reportWebVitals';
 
